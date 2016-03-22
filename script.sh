cc9125eae8e098b395b27eda732e54a6d0c02fdb
Charles Carlfjord <gauz93@gmail.com>
2016-03-22 21:58:09 +0100

    rename + sudo change

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 
 XBINDKEYS_OK=$(dpkg-query -W --showformat='${Status}\n' xbindkeys|grep "install ok installed")
 
-sudo -s
+sudo -u root
 cp ./unbind_ehci /etc/initramfs-tools/scripts/init-top/unbind_ehci &&
 chmod a+x etc/initramfs-tools/scripts/init-top/unbind_ehci &&
 cp ./10_disable-ehci.rules /etc/udev/rules.d/10_disable-ehci.rules &&
@@ -14,5 +14,5 @@ update-grub &&
 exit
 
 if [[ $XBINDKEYS_OK == "install ok installed" ]]; then
-  
+
 fi
