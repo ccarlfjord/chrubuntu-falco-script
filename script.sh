432d77f1d7e392abc1155740027d0387f3f9aba7
Charles Carlfjord <gauz93@gmail.com>
2016-04-06 23:03:58 +0200

    fixed paths

--- script.sh
+++ script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # Chromebook script for Ubuntu based distros on HP Chromebook 14 "Falco"
-SOURCE="`pwd`/source"
+SOURCE="`dirname $0`/source"
 
 XBINDKEYS_OK=$(dpkg-query -W --showformat='${Status}\n' xbindkeys|grep "install ok installed")
 
 sudo -u root
-cp $F/unbind_ehci /etc/initramfs-tools/scripts/init-top/unbind_ehci &&
+cp $SOURCE/unbind_ehci /etc/initramfs-tools/scripts/init-top/unbind_ehci &&
 chmod a+x etc/initramfs-tools/scripts/init-top/unbind_ehci &
-cp $F/10_disable-ehci.rules /etc/udev/rules.d/10_disable-ehci.rules &&
+cp $SOURCE/10_disable-ehci.rules /etc/udev/rules.d/10_disable-ehci.rules &&
 update-initramfs -k all -u &&
 cp /etc/default/grub /etc/default/grub.bak &&
-cp $F/grub /etc/default/grub &&
+cp $SOURCE/grub /etc/default/grub &&
 update-grub &&
 exit
 
 if [[ $XBINDKEYS_OK == "install ok installed" ]]; then
-  cp $F/.xbindkeysrc $HOME/.xbindkeysrc &
-  cp $F/.Xmodmap $HOME/.Xmodmap
+  cp $SOURCE/.xbindkeysrc $HOME/.xbindkeysrc &
+  cp $SOURCE/.Xmodmap $HOME/.Xmodmap
 fi
