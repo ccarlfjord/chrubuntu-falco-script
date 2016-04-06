7c4edd1020c868127ecbe6979bffd95486523076
Charles Carlfjord <gauz93@gmail.com>
2016-04-06 23:12:44 +0200

    using sudo only where needed

--- script.sh
+++ script.sh
@@ -4,15 +4,13 @@ SOURCE="`dirname $0`/source"
 
 XBINDKEYS_OK=$(dpkg-query -W --showformat='${Status}\n' xbindkeys|grep "install ok installed")
 
-sudo -u root
-cp $SOURCE/unbind_ehci /etc/initramfs-tools/scripts/init-top/unbind_ehci &&
-chmod a+x etc/initramfs-tools/scripts/init-top/unbind_ehci &
-cp $SOURCE/10_disable-ehci.rules /etc/udev/rules.d/10_disable-ehci.rules &&
-update-initramfs -k all -u &&
-cp /etc/default/grub /etc/default/grub.bak &&
-cp $SOURCE/grub /etc/default/grub &&
-update-grub &&
-exit
+sudo cp $SOURCE/unbind_ehci /etc/initramfs-tools/scripts/init-top/unbind_ehci &&
+sudo chmod a+x etc/initramfs-tools/scripts/init-top/unbind_ehci &
+sudo cp $SOURCE/10_disable-ehci.rules /etc/udev/rules.d/10_disable-ehci.rules &&
+sudo update-initramfs -k all -u &&
+sudo cp /etc/default/grub /etc/default/grub.bak &&
+sudo cp $SOURCE/grub /etc/default/grub &&
+sudo update-grub &&
 
 if [[ $XBINDKEYS_OK == "install ok installed" ]]; then
   cp $SOURCE/.xbindkeysrc $HOME/.xbindkeysrc &
