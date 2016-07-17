2f117622b899f26f31f45682c7c9e9527aedbd69
Charles Carlfjord <gauz93@gmail.com>
2016-07-17 17:44:13 +0200

    added systemd service fix for sleep

--- script.sh
+++ script.sh
@@ -17,6 +17,10 @@ then
   sudo cp /etc/default/grub /etc/default/grub.bak; printf "Creating backup of grub file to /etc/default/grub.bak...\n" &&
   sudo cp $SOURCE/grub /etc/default/grub; printf "Copying new grub file...\n" &&
   sudo update-grub
+  sudo cp $SOURCE/suspend-device-fix.sh /usr/local/sbin/suspend-device-fix.sh; printf "Creating suspend-fix service \n"
+  sudo chmod +x /usr/local/sbin/suspend-device-fix.sh
+  sudo cp $SOURCE/suspend-fix.service /etc/systemd/system/suspend-fix.service
+  sudo systemctl enable suspend-fix.service
 else
   printf "xbindkeys not installed, installing...\n"; sudo apt-get install xbindkeys && $BASE_DIR/script.sh
 fi
